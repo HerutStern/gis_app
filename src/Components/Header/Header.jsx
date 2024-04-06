398b42720877afb43170ba4bdd0d19f66174169c
HERUT <herut.shtern@gmail.com>
2024-04-07 01:51:27 +0300

    intial

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import './Header.css'
+
+const Header = () => {
+  return(
+    <>
+    <div style={{display: 'flex', flexDirection: 'row'}}>
+      <img src="https://i.ibb.co/7rQzP2g/Doc1.jpg" alt="Doc1" border="0" width={'150em'} height={'auto'}/>
+    <h1 >
+      GIS APP
+    </h1>
+    </div>
+    
+    <h2>
+      Click on a city for information
+    </h2>
+    </>
+
+  )
+}
+
+export default Header
